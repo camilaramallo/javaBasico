320c56b2b446acedb75e414ba293484951154937
camilaramallo <lagrimaazul70@gmail.com>
2022-07-08 19:17:31 -0300

    tarea EstructuraRepetitiva

---
+++ src/EstructurasRepetitivas.java
@@ -0,0 +1,18 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+public class EstructurasRepetitivas {
+    public static void main(String[] args) {
+        String[] arreglo = {"Analia", "Victoria","Fran"};
+        //ArrayList<String> array = new ArrayList<>();
+        for(String a : arreglo) {
+            System.out.print(" " +a);
+        }
+
+    }
+
+
+}
